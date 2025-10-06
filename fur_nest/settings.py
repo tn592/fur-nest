33d4cd0fe1bb4eafec52b2af35413a0bf1a47302
tanzina <tn59249@yahoo.com>
2025-10-06 23:12:07 +0600

    Frontend URL added in Djoser

--- fur_nest/settings.py
+++ fur_nest/settings.py
@@ -174,6 +174,9 @@ SIMPLE_JWT = {
 }
 
 DJOSER = {
+    "EMAIL_FRONTEND_PROTOCOL": config("FRONTEND_PROTOCOL"),
+    "EMAIL_FRONTEND_DOMAIN": config("FRONTEND_DOMAIN"),
+    "EMAIL_FRONTEND_SITE_NAME": "FurNest",
     "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
     "ACTIVATION_URL": "activate/{uid}/{token}",
     "SEND_ACTIVATION_EMAIL": True,
